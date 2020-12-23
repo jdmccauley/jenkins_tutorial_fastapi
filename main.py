7f1075dc5f23b7af5d4e3b8c9644f60630a15e1d
Joshua McCauley <JDMcCauley@lbl.gov>
2020-12-23 12:52:51 -0800

    Small change.

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/helloworld")
 async def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/worldhello")
+async def world_hello():
+    return {"message": "World Hello"}
